857d3f9bbeb32d81dcec933de85953978c6a7a00
ozwaldo <ozwaldo@gmail.com>
2019-10-28 07:49:21 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog de Tecnología</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="row d-flex align-items-center">
+                <div class="col-4"></div>
+                <div class="col-4 text-center">
+                    <a href="#" class="blog-logo">
+                        Blog
+                    </a>
+                </div>
+                <div class="col-4"></div>
+            </div>
+        </header>
+        <div class="nav-blog">
+            <nav class="nav d-flex justify-content-between">
+                <a href="#" class="p-2 text-muted">Inicio</a>
+                <a href="#" class="p-2 text-muted">Categoria 1</a>
+                <a href="#" class="p-2 text-muted">Categoria 2</a>
+                <a href="#" class="p-2 text-muted">Categoria 3</a>
+            </nav>
+        </div>
+    
+    </div> <!-- /container -->
+</body>
+</html>
