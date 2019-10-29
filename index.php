9eb4f4d1b0c6345299005b9dec625e1508bef4a0
abc <abc@email.com>
2019-10-29 13:58:34 -0600

    Ok At1

--- index.php
+++ index.php
@@ -10,10 +10,7 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/estilo.css">
-
-
 </head>
-
 <body>
     <div class="container">
         <header>
@@ -158,16 +155,23 @@
         </div>
     </div> <!-- /container -->
     <footer class="blog-footer mt-5">
-        <div class="container text-center">
-            <ul class="social-network social-circle">
-                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-            </ul>
+        <div class="container">
+            <div class="row justify-content-between">                
+                <div class="col-4">
+                    <div class="footer-copyright text-center py-3">
+                        Todos los derechos Reservados - 2019
+                    </div>
+                </div>
+                <div class="col-4">
+                    <ul class="social-network social-circle">
+                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="footer-copyright text-center py-3">
-            Todos los derechos Reservados - 2019
-        </div>
+        
     </footer>
 </body>
 
